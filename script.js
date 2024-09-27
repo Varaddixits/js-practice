24668bbc461156532c8df89c569ccc93b5d8b018
varad <varaddixit11@gmail.com>
2024-09-27 14:33:55 +0530

    ff

---
+++ script.js
@@ -0,0 +1,48 @@
+// function add(num 1, num 2){
+
+// return num 1 + num 2;
+// }
+
+// console.log(add)
+
+
+//5
+// function sum(num1, num2) {
+//     return (num1 + num2) < 100;
+// }
+
+
+// console.log(sum(30, 40)); 
+// console.log(sum(60, 50)); 
+
+
+//2
+// function incrementNumber(num) {
+//     return num + 1;
+// }
+
+
+// console.log(incrementNumber(5)); 
+// console.log(incrementNumber(-6));
+
+//3
+// function TriangleArea(base, height) {
+//     return 0.5 * base * height;
+// }
+
+
+// console.log(TriangleArea(10,20)); 
+// console.log(TriangleArea(7, 3));  
+
+//6
+// function isEvenOrOdd(num) {
+//     if (num % 2 === 0) {
+//         return 'Even';
+//     } else {
+//         return 'Odd';
+//     }
+// }
+
+
+// console.log(isEvenOrOdd(4)); 
+// console.log(isEvenOrOdd(7)); 
